cca531112ba936048891ea1abb5ba582ee7bd1ef
BS <bens640@gmail.com>
2022-03-18 14:13:06 -0400

    cleaned up index.js and remove unneeded dependencies from package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const dayjs = require('dayjs');
 
 exports.handler =  async function(event, context) {
-    var now = dayjs()
-    console.log('+*+++*+*+*+*+*+*+*+*+*+**+*++*+*')
-    console.log('EVENT OCCURRED!')
-    console.log(`Message created at ${now}`)
-    console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-    console.log('+*+++*+*+*+*+*+*+*+*+*+**+*++*+*')
+    var now = dayjs();
+    console.log('+*+++*+*+*+*+*+*+*+*+*+**+*++*+*');
+    console.log('EVENT OCCURRED!');
+    console.log(`Message created at ${now}`);
+    console.log("EVENT: \n" + JSON.stringify(event, null, 2));
+    console.log('+*+++*+*+*+*+*+*+*+*+*+**+*++*+*');
     return context.logStreamName
 }
 
